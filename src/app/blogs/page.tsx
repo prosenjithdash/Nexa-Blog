004592a7f3af994ac80201af90f226601a6127e7
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-04-13 02:36:01 +0600

    Client side rendring done

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,19 +1,29 @@
+"use client";
 import BlogCard from '@/components/ui/BlogCard';
+import Spinner from '@/components/ui/Spinner';
+import { useGetBlogsQuery } from '@/redux/apis/blogs.slice';
 import { Blog } from '@/types';
-import { Metadata } from 'next';
+// import { Metadata } from 'next';
 import React from 'react';
 
-export const metadata:Metadata = {
-    title:'NexaBlog | Blogs'
-}
+// export const metadata:Metadata = {
+//     title:'NexaBlog | Blogs'
+// }
 
 
-const BlogsPage = async () => {
-    const res = await fetch('http://localhost:8000/blogs', {
-        cache:'no-store'
-    })
-    const blogs = await res.json()
-    console.log(blogs)
+const BlogsPage =  () => {
+    // const res = await fetch('http://localhost:8000/blogs', {
+    //     cache:'no-store'
+    // })
+    // const blogs = await res.json()
+    // console.log(blogs)
+
+    const { data:blogs, isLoading } = useGetBlogsQuery({});
+    // console.log(data)
+    
+    if (isLoading) {
+        return <Spinner/>
+    }
     return (
         <div className='w-[90%] mx-auto'>
            <h1 className="text-3xl text-center my-5 font-bold">
@@ -27,8 +37,9 @@ const BlogsPage = async () => {
             </p>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-5 ">
                 {
-                    blogs.map((blog: Blog)=><BlogCard key={blog.id} blog={blog}/>)
+                    blogs?.map((blog: Blog)=>(<BlogCard key={blog.id} blog={blog}/>))
                 }
+                
             </div>
         </div>
     );
